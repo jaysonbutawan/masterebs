3fc45d8c3040c4507293b13783c968464f4b188f
jaysonbutawan <159876164+jaysonbutawan@users.noreply.github.com>
2026-03-09 21:20:55 +0800

    aaa

--- app/Models/OrderItems.php
+++ app/Models/OrderItems.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItems extends Model
 {
-    //
+        protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price'
+    ];
+
+    protected $table = 'order_items';
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\OrderItems;
 
 class Product extends Model
 {
-    //
+     protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'stock'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItems::class);
+    }
 }
