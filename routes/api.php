e299d095b7680760425773b16d9b0010e89a0cf0
LaVenganzaDelLadron <darkglitch5417@gmail.com>
2026-03-13 19:48:13 +0800

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,35 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('room')->name('room.')->group(function () {
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::get('/health', [AuthController::class, 'health'])->name('health');
+Route::name('auth.')->controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login')->name('login');
+    Route::post('/register', 'register')->name('register');
+    Route::get('/health', 'health')->name('health');
+});
 
-    Route::middleware('api.auth')->group(function () {
-        Route::get('/profile/{id}', [AuthController::class, 'profile'])->name('profile');
+Route::middleware('api.auth')->group(function () {
+    Route::prefix('profile')->name('auth.')->group(function () {
+        Route::get('/{id}', [AuthController::class, 'profile'])->name('profile');
+    });
+
+    Route::name('catalog.')->group(function () {
+        Route::apiResource('categories', CategoryController::class);
+        Route::apiResource('products', ProductController::class);
+    });
+
+    Route::name('sales.')->group(function () {
+        Route::apiResource('orders', OrderController::class);
+        Route::apiResource('order-items', OrderItemController::class);
+    });
+
+    Route::name('access.')->group(function () {
+        Route::apiResource('roles', RoleController::class);
     });
 });
